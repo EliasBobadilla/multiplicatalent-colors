efa41d71de8c44e1482fb1f3f7e987885f99a846
eliasbobadilla <elias.bobadilla@gmail.com>
2021-11-28 13:31:53 -0500

    enable cors

--- src/routes/swagger.js
+++ src/routes/swagger.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('../../swagger.json')
+const cors = require('cors')
 
 const router = Router()
 
 router.use('/', swaggerUi.serve)
-router.get('/', swaggerUi.setup(swaggerDocument))
+router.get('/', cors(), swaggerUi.setup(swaggerDocument))
 
 module.exports = router
